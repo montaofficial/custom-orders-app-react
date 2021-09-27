0c5186c5d9e418ec75d383e41bec6151b7f3b7e0
Simone Montanari <simone@tokiostudio.it>
2021-09-27 12:09:27 +0200

    added tuttoaddestra tag in TableOrders

--- src/components/TableOrders.jsx
+++ src/components/TableOrders.jsx
@@ -122,7 +122,7 @@ class tableOrders extends Component {
             </div>
             <div className="col-auto allign-right-title">
               <div className="row">
-                <div className="col-auto">
+                <div className="col-auto tuttoaddestra">
                   <div className="menu-icon">
                     <i
                       className="fas fa-user-plus"
