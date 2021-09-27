3ffc96a29afda5a13e95326429c5056bbe290cd1
salafede <saladino97@gmail.com>
2021-09-27 11:10:17 +0200

    Added description under action button

--- src/components/common/Order.jsx
+++ src/components/common/Order.jsx
@@ -15,49 +15,50 @@ class Order extends Component {
     let state = order.currentState;
     let page = this.state.page;
     let cl = "";
+    let text = "";
     if (state === "Waiting confirmation" && page === "cassa") {
       cl = "fas fa-check-circle cursor-pointer";
-    }
-    if (state === "" && page === "cassa") {
-      cl = "fas fa-check-circle cursor-pointer";
+      text = "Conferma";
     }
     if (state === "Deleted" && page === "cassa") {
       cl = "fas fa-check-circle cursor-pointer";
+      text = "Recupera";
     }
     if (state === "Confirmed" && page === "cucina") {
       cl = "fas fa-bacon cursor-pointer";
+      text = "Prepara";
     }
     if (state === "In preparation" && page === "cucina") {
       cl = "fas fa-check-circle cursor-pointer";
+      text = "Pronto";
     }
     if (state === "Ready" && page === "cassa") {
       cl = "fas fa-check-circle cursor-pointer";
+      text = "Consegnati";
     }
 
-    return cl;
+    return [cl, text];
   };
 
   handleDeleteButton = (order) => {
     let state = order.currentState;
     let page = this.state.page;
     let cl = "";
+    let text = "";
     if (state === "Waiting confirmation" && page === "cassa") {
       cl = "fas fa-times-circle cursor-pointer";
+      text = "Elimina";
     }
     if (state === "Confirmed" && page === "cassa") {
       cl = "fas fa-times-circle cursor-pointer";
+      text = "Elimina";
     }
     if (state === "Waiting confirmation" && page === "tableOrders") {
       cl = "fas fa-times-circle cursor-pointer";
+      text = "Annulla";
     }
 
-    return cl;
-  };
-
-  handleCloseTableButton = (table) => {
-    let page = this.state.page;
-    if (page === "cassa") return "pappa";
-    return "";
+    return [cl, text];
   };
 
   handleIcon = (type) => {
@@ -75,46 +76,59 @@ class Order extends Component {
                 <div className="col-auto order-section-title">
                   Tavolo {table.number}
                 </div>
-                {this.handleCloseTableButton(table) ? (
-                  <div className="col-auto"></div>
-                ) : null}
+                <div className="col-auto"></div>
               </div>
-              {table.orders.map((order, key2) => (
-                <div className="order-section-element">
-                  <div className="row justify-content-between" key={key2}>
-                    <div className="col-auto   order-section-title">
-                      <i className={this.handleIcon(order.type)} />
-                      {order.type}
-                      {this.state.page === "cassa" ||
-                      (this.state.page === "tableOrders" &&
-                        order.currentState !== "Waiting confirmation")
-                        ? " - " + order.price + "  €"
-                        : null}
+              {table.orders.map((order, key2) => {
+                const [button1, button1Text] = this.handleConfirmButton(order);
+                const [button2, button2Text] = this.handleDeleteButton(order);
+
+                return (
+                  <div className="order-section-element">
+                    <div className="row justify-content-between" key={key2}>
+                      <div className="col-auto   order-section-title">
+                        <i className={this.handleIcon(order.type)} />
+                        {order.type}
+                        {this.state.page === "cassa" ||
+                        (this.state.page === "tableOrders" &&
+                          order.currentState !== "Waiting confirmation")
+                          ? " - " + order.price + "  €"
+                          : null}
+                      </div>
+                      <div className="col-auto row">
+                        {button1 ? (
+                          <div className="col-auto admin-button">
+                            <div className="menu-icon">
+                              <i
+                                className={button1}
+                                onClick={() =>
+                                  this.props.onAction(order, "btn1")
+                                }
+                              />
+                            </div>
+                            <div className="menu-subtitle">{button1Text}</div>
+                          </div>
+                        ) : null}
+                        {button2 ? (
+                          <div className="col-auto admin-button">
+                            <div className="menu-icon">
+                              <i
+                                className={this.handleDeleteButton(order)}
+                                onClick={() =>
+                                  this.props.onAction(order, "btn2")
+                                }
+                              />
+                            </div>
+                            <div className="menu-subtitle">{button2Text}</div>
+                          </div>
+                        ) : null}
+                      </div>
                     </div>
-                    <div className="col-auto row">
-                      {this.handleConfirmButton(order) ? (
-                        <div className="col-auto admin-button">
-                          <i
-                            className={this.handleConfirmButton(order)}
-                            onClick={() => this.props.onAction(order, "btn1")}
-                          />
-                        </div>
-                      ) : null}
-                      {this.handleDeleteButton(order) ? (
-                        <div className="col-auto admin-button">
-                          <i
-                            className={this.handleDeleteButton(order)}
-                            onClick={() => this.props.onAction(order, "btn2")}
-                          />
-                        </div>
-                      ) : null}
+                    <div className="col white">
+                      {this.handleIngredients(order)}
                     </div>
                   </div>
-                  <div className="col white">
-                    {this.handleIngredients(order)}
-                  </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
           </div>
         ))}{" "}
