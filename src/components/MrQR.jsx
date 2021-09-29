4a29cbd5a4482dba722447e2a069914d4229f64e
Simone Montanari <simone@tokiostudio.it>
2021-09-29 09:32:01 +0200

    fixed baseUrl

--- src/components/MrQR.jsx
+++ src/components/MrQR.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import QrCode from "./common/QrCode";
 const baseUrl = "https://custom-orders.smontanari.com/api/";
-const frontBaseUrl = "http://192.168.1.84:3000/";
+const frontBaseUrl = "https://custom-orders.smontanari.com/";
 
 class MrQR extends Component {
   constructor(props) {
