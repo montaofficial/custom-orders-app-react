6ea5a19984932383c42bef899b7c21166dbb21d7
Simone Montanari <simone@tokiostudio.it>
2021-10-06 16:54:48 +0200

    implemented authentication

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
 
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
 import NotFound from "./components/NotFound";
 import Gestione from "./components/Gestione";
 import Cucina from "./components/Cucina";
 import GestioneWaiter from "./components/GestioneWaiter";
+import Login from "./components/Login";
 
 import logo from "./logo.svg";
 import "./App.css";
 import Table from "./components/Table";
 
-function App() {
-  return (
-    <Router>
-      <div className="container-flex noselect">
-        <Switch>
-          <Route path="/:idRistorante/gestione" component={Gestione} />
-          <Route path="/:idRistorante/waiter" component={GestioneWaiter} />
-          <Route path="/:idRistorante/cucina" component={Cucina} />
-          <Route path="/:idRistorante/:idTavolo/" component={Table} />
-          <Route path="/">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
 
+const AuthRoute = props => {
+  const { type } = props;
+  const isAuthUser = localStorage.getItem('custom-orders-token');
+  if (type === "login" && isAuthUser) return <Redirect to="/" />;
+  else if (type === "private" && !isAuthUser) return <Redirect to="/login" />;
+
+  return <Route {...props} />;
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <Router>
+        <div className="container-flex noselect">
+          <Switch>
+          <AuthRoute path="/login" type="login"><Login callBack={()=>{this.updateToken()}}/></AuthRoute>
+          <AuthRoute path="/:idRistorante/gestione" type="private" component={Gestione}/>
+            <Route path="/:idRistorante/gestione" component={Gestione}/>
+            <AuthRoute path="/:idRistorante/waiter" type="private" component={GestioneWaiter} />
+            <AuthRoute path="/:idRistorante/cucina" type="private" component={Cucina} />
+            <Route path="/:idRistorante/:idTavolo/" component={Table} />
+            <Route path="/">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('custom-orders-token');
+    this.setState({ user: null});
+  }
+  
+  updateToken() {
+    let user;
+      const token = localStorage.getItem('custom-orders-token');
+      if (token) user = jwt_decode(token);
+  
+      this.setState({user: user});
+  }
+}
+ 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import '../login.css'
+
+const baseUrl = "https://custom-orders.smontanari.com/api/";
+
+class Login extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: false,
+        name: "",
+        password: "",
+        loggedIn: false,
+        visiblePassword: false
+      };
+  }
+
+  componentDidMount() {
+      
+  }
+
+  keyDown (event){
+    if(event.keyCode === '13'){
+      this.login();
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {
+          this.state.loggedIn? <Redirect to={{pathname: "/" ,state: { from: null }}}/> : null
+        }
+      <div className="container" onKeyDown={e => {this.keyDown(e)}}>
+        <div className="row justify-content-md-center">
+          <div className="col-12 col-md-9 col-lg-8">
+            <div className=" login-card">
+              <div className="row justify-content-center">
+                <div className="col-auto">
+                <img src="/logo-sham-low.svg" className="logo-login" alt="Logo PGE"></img>
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-12">
+                <h3 className="login-title">PGE ADMIN</h3>
+                </div>
+              </div>
+              <div  className="row">
+                <div className="col-12">
+                  <div className="description-login">Username</div>
+                </div>
+              </div>
+              <div  className="row justify-content-center">
+                <div className="col-12 pb-2 pt-1">
+                  <div className="wrap-input100 validate-input">
+                    <input className="input100" type="text" id="nome" name="nome"  onKeyDown={e => {this.keyDown(e)}} value={this.state.name} onChange={(event)=> {this.setState( Object.assign({}, this.state, { name: event.target.value }))}}/>
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100"></span>
+                  </div>
+                </div>
+              </div>
+              <div  className="row">
+                <div className="col-12">
+                  <div className="description-login">Password</div>
+                </div>
+              </div>
+              <div  className="row justify-content-center">
+                <div className="col-12 pb-2 pt-1">
+                 <div className="wrap-input100 validate-input">
+                 {this.state.visiblePassword? <span className="symbol-input100" onClick={()=>{this.setState({visiblePassword: ! this.state.visiblePassword})}}><i className="fa fa-eye-slash" aria-hidden="true"></i></span>
+                 :<span className="symbol-input100" onClick={()=>{this.setState({visiblePassword: ! this.state.visiblePassword})}}><i className="fa fa-eye" aria-hidden="true"></i></span>
+                    }<input className="input100" type={this.state.visiblePassword? "text" : "password"} name="id" id="id"  onKeyDown={e => {this.keyDown(e)}} value={this.state.password} onChange={(event)=> {this.setState( Object.assign({}, this.state, { password: event.target.value }))}}/>
+                    <span className="focus-input100"></span>
+                  </div>
+                </div>
+              </div>
+              <div  className="row justify-content-center">
+                <div className="col-12 pb-2 pt-1">
+                <div className="container-login100-form-btn">
+                  <button className="login100-form-btn submit2" onClick={()=>{this.login()}} onKeyDown={e => {this.keyDown(e)}} type="submit">
+                  {this.state.loading ? <img src="/logo-sham-low.svg" alt="loading..."/> : 'Accedi'}
+                  </button>
+                 </div>
+                </div>
+              </div>
+              {this.state.error ? <div  className="row justify-content-center">
+                <div className="col-12 pt-3">
+                  <div className="alert alert-danger w3-animate-fading">
+                    <div>
+                    <i className="fas fa-exclamation-triangle"></i> I tuoi dati sono sbagliati! Riprova.
+                    </div>
+                  </div>
+                </div>
+              </div> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+      );
+  }
+  async login () {
+    let response = await fetch(`${baseUrl}/auth`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          password: this.state.password.trim(),
+          username: this.state.name.trim(),
+          browser: {
+            codeName:navigator.appCodeName,
+            name: navigator.vendor,
+            version: navigator.appVersion,
+            cookies: navigator.cookieEnabled,
+            platform: navigator.platform,
+            userAgent: navigator.userAgent
+          }
+      })
+    });
+    
+    if (!response.ok) {
+      response.text().then(res => console.log(res));
+      this.setState( Object.assign({}, this.state, {
+        error: true,
+        loading: false
+      }));
+      setTimeout(()=>{
+        this.setState( Object.assign({}, this.state, {
+          error: false
+        }))
+      },6000);
+      return;
+    }
+    let data = {};
+    try {data = await response.json()}
+    catch (e) {console.log(e.message)}
+    if (data && data.token)
+    localStorage.setItem('custom-orders-token', data.token);
+    this.props.callBack();
+
+  }
+
+}
+
+export default Login;
